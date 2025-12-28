2924f249dfb0d5467efd7692d517538a09b2a228
deepa <deepacleetus182@gmail.com>
2025-12-28 18:18:51 +0530

    vercel

--- foodie/vite.config.js
+++ foodie/vite.config.js
@@ -6,5 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   base: '/',
+  build: {
+    outDir: 'dist'    // REQUIRED
+  }
   
 })
+
+
