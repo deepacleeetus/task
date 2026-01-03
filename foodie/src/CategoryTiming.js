4bba6cb8b3b6f3ea251865e4628ad3dc4c538489
Deepa Cleeetus <72685377+deepacleeetus@users.noreply.github.com>
2026-01-03 11:45:01 +0530

    Delete foodie directory

--- foodie/src/CategoryTiming.js
+++
@@ -1,9 +0,0 @@
-export const category_timings = {
-  breakfast: { start: 8, end: 11 },
-  lunch: { start: 12, end: 15 },
-  dinner: { start: 19, end: 23 },
-  snacks: { start: 16, end: 18 },
-  pasta: { start: 12, end: 22 },
-  soups: { start: 11, end: 21 },
-  main_course: { start: 12, end: 23 },
-};
